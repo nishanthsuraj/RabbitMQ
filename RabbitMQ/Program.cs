54e1abd1d0edcc911d0e6fdeb7730468494c87a4
nishanthsuraj <nishanthsuraj@gmail.com>
2024-03-01 09:12:47 +0530

    Add project files.

---
+++ RabbitMQ/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System.Text;
+
+const string Url = "amqp://guest:guest@localhost:5672";
+const string QueueName = "demo-queue";
+SingleProducer();
+
+
+void SingleProducer()
+{
+    ConnectionFactory factory = new ConnectionFactory { Uri = new Uri(Url) };
+    using IConnection connection = factory.CreateConnection();
+    using IModel channel = connection.CreateModel();
+    channel.QueueDeclare(queue: QueueName, durable: true,
+        exclusive: false, autoDelete: false, arguments: null);
+    (string Name, string Message) Message = (Name: "Producer", Message: "Hello!");
+    byte[] body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(Message));
+
+    channel.BasicPublish(exchange: "", routingKey: QueueName, basicProperties: null, body: body);
+}
---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+const string Url = "amqp://guest:guest@localhost:5672";
+const string QueueName = "demo-queue";
+SingleConsumer();
+
+void SingleConsumer()
+{
+    ConnectionFactory factory = new ConnectionFactory { Uri = new Uri(Url) };
+    using IConnection connection = factory.CreateConnection();
+    using IModel channel = connection.CreateModel();
+    channel.QueueDeclare(queue: QueueName, durable: true,
+        exclusive: false, autoDelete: false, arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (sender, e) =>
+    {
+        byte[] body = e.Body.ToArray();
+        string message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(message);
+    };
+    channel.BasicConsume(queue: QueueName, autoAck: true, consumer: consumer);
+    Console.ReadLine();
+}
